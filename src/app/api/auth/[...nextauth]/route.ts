bb76228bde4ec66dbd377eef0a41b126c12994e1
Nese Yildirim <neseyild@student.arteveldehs.be>
2023-11-15 21:06:49 +0100

    api connection

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,22 @@
 import { authOptions } from "@/libs/auth";
 import NextAuth from "next-auth";
 
-const handler = NextAuth(authOptions)
+const apiUrl = "https://whimsical-activity-841245cb28.strapiapp.com"; // Vervang dit met de daadwerkelijke URL van je Strapi-backend
 
-export {handler as GET, handler as POST}
+const handler = NextAuth({
+  ...authOptions, // Voeg eventuele bestaande opties toe vanuit authOptions
+
+  // Voeg nieuwe opties toe of overschrijf bestaande opties
+  providers: [
+    ...authOptions.providers, // Voeg eventuele bestaande providers toe vanuit authOptions
+    // Hier kun je aanvullende providers configureren indien nodig
+  ],
+  
+  // Voeg de nieuwe optionele baseUrl toe aan site die overeenkomt met je Strapi-backend
+  baseUrl: apiUrl,
+
+  // Andere algemene NextAuth-configuraties hier ...
+  // ...
+});
+
+export { handler as GET, handler as POST };
